14c2ac9d1d196e80973f63997a147c5269369ba6
Na3ar-17 <gavruk2017@gmail.com>
2024-07-04 17:59:10 +0200

    enabled cors and set global prefix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
