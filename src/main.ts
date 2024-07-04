ebb02e3b37d7b80f0a53170eba0615078b41527d
Na3ar-17 <gavruk2017@gmail.com>
2024-07-04 18:50:17 +0200

    used cookie parser in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
   app.setGlobalPrefix('api');
   app.enableCors({
     credentials: true,
+    exposedHeaders: 'set-cookie',
   });
-  await app.listen(3000);
+  await app.listen(4200);
 }
 bootstrap();
