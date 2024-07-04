d2748fb53f13d8c83435be4527e924464c87b7e3
Na3ar-17 <gavruk2017@gmail.com>
2024-07-04 19:03:34 +0200

    installed graphql-tools

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TmpModule } from './tmp/tmp.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [TmpModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+    }),
+  ],
 })
 export class AppModule {}
