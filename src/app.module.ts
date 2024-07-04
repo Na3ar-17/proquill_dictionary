1801a0191123f9f3143bf36d446e8073a1fe4397
Na3ar-17 <gavruk2017@gmail.com>
2024-07-04 18:59:26 +0200

    installed graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TmpModule } from './tmp/tmp.module';
 
 @Module({
-  imports: [],
+  imports: [TmpModule],
   controllers: [AppController],
   providers: [AppService],
 })
