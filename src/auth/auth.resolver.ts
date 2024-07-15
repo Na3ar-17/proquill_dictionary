294b8d66526b17bcf5564b25ae05d07ab6c2e704
Na3ar-17 <gavruk2017@gmail.com>
2024-07-15 18:26:11 +0200

    created all methods in auth resolver

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,7 +1,71 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
-
-@Resolver()
+import { User } from '../user/entities/user.entity';
+import {
+  Req,
+  Res,
+  UnauthorizedException,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateUserInput } from 'src/user/dto/create-user.input';
+import { Request, Response } from 'express';
+@Resolver(() => User)
 export class AuthResolver {
   constructor(private readonly authService: AuthService) {}
+
+  @Mutation(() => User)
+  @UsePipes(new ValidationPipe())
+  async login(
+    @Args('login') dto: CreateUserInput,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { refreshToken, ...response } = await this.authService.login(dto);
+    this.authService.addRefreshTokenToResponse(res, refreshToken);
+
+    return response;
+  }
+
+  @Mutation(() => User)
+  @UsePipes(new ValidationPipe())
+  async register(
+    @Args('login') dto: CreateUserInput,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const { refreshToken, ...response } = await this.authService.register(dto);
+    this.authService.addRefreshTokenToResponse(res, refreshToken);
+
+    return response;
+  }
+
+  @Mutation(() => User)
+  @UsePipes(new ValidationPipe())
+  async getNewTokens(
+    @Res({ passthrough: true }) res: Response,
+    @Req() req: Request,
+  ) {
+    const refreshTokenFromCookies =
+      req.cookies[this.authService.REFRESH_TOKEN_NAME];
+
+    if (!refreshTokenFromCookies) {
+      this.authService.removeRefreshTokenFromResponse(res);
+      throw new UnauthorizedException('Refresh token to passed');
+    }
+
+    const { refreshToken, ...response } = await this.authService.getNewTokens(
+      refreshTokenFromCookies,
+    );
+
+    this.authService.addRefreshTokenToResponse(res, refreshToken);
+
+    return response;
+  }
+
+  @Mutation(() => User)
+  @UsePipes(new ValidationPipe())
+  async logout(@Res({ passthrough: true }) res: Response) {
+    this.authService.removeRefreshTokenFromResponse(res);
+
+    return true;
+  }
 }
