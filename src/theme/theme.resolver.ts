75265b02765f52ce259e44ca8d1ac2e325fa54b0
Na3ar-17 <gavruk2017@gmail.com>
2024-07-17 16:02:20 +0200

    created mutation for create theme

--- src/theme/theme.resolver.ts
+++ src/theme/theme.resolver.ts
@@ -1,10 +1,18 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { ThemeService } from './theme.service';
 import { Theme } from './entities/theme.entity';
 import { CreateThemeInput } from './dto/create-theme.input';
 import { UpdateThemeInput } from './dto/update-theme.input';
+import { Auth } from 'src/auth/decorators/auth.decorator';
 
 @Resolver(() => Theme)
 export class ThemeResolver {
   constructor(private readonly themeService: ThemeService) {}
+
+  @Mutation(() => Theme)
+  // @UsePipes(new ValidationPipe())
+  @Auth()
+  async create(@Args('createThemeInput') createThemeInput: CreateThemeInput) {
+    return await this.themeService.create(createThemeInput);
+  }
 }
