fbb8a520a99bf143552aa0740ec6349eaa7729d8
Na3ar-17 <gavruk2017@gmail.com>
2024-07-18 12:52:57 +0200

    created content entity

--- src/content/entities/content.entity.ts
+++ src/content/entities/content.entity.ts
@@ -1,4 +1,31 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
 
 @ObjectType()
-export class Content {}
+export class Content {
+  @Field(() => String)
+  id: string;
+
+  @Field(() => Date)
+  createdAt: Date;
+
+  @Field(() => Date)
+  updatedAt: Date;
+
+  @Field(() => String)
+  themeId: string;
+
+  @Field(() => String)
+  sentence: string;
+
+  @Field(() => String)
+  translation: string;
+
+  @Field(() => String, { nullable: true })
+  transcription?: string;
+
+  @Field(() => [String])
+  exampleSentences: string[];
+
+  @Field(() => String, { nullable: true })
+  imageUrl?: string;
+}
