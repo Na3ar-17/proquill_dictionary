9228559eb3a54160cd9f57df85f3856d7bd85e45
Na3ar-17 <gavruk2017@gmail.com>
2024-07-21 12:42:36 +0200

    created query getLearningProgress

--- src/learning-progress/learning-progress.resolver.ts
+++ src/learning-progress/learning-progress.resolver.ts
@@ -1,12 +1,22 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { LearningProgressService } from './learning-progress.service';
 import { LearningProgress } from './entities/learning-progress.entity';
-import { CreateLearningProgressInput } from './dto/create-learning-progress.input';
 import { UpdateLearningProgressInput } from './dto/update-learning-progress.input';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { CurrentUser } from 'src/auth/decorators/user.decorator';
 
 @Resolver(() => LearningProgress)
 export class LearningProgressResolver {
   constructor(
     private readonly learningProgressService: LearningProgressService,
   ) {}
+
+  @Query(() => LearningProgress, { name: 'getLearningProgress' })
+  @Auth()
+  async findOne(
+    @CurrentUser('id') userId: string,
+    @Args('themeId') themeId: string,
+  ) {
+    return await this.learningProgressService.findOne(themeId, userId);
+  }
 }
