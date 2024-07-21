f1ff681dd68e9dea7dfc2a06a6a171796c00fb91
Na3ar-17 <gavruk2017@gmail.com>
2024-07-21 12:43:55 +0200

    updated lerning progres update input

--- src/learning-progress/dto/update-learning-progress.input.ts
+++ src/learning-progress/dto/update-learning-progress.input.ts
@@ -3,8 +3,8 @@ import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateLearningProgressInput extends CreateLearningProgressInput {
-  @Field(() => Int)
-  wordsLearned: number;
+  @Field(() => Int, { nullable: true })
+  wordsLearned?: number;
 
   @Field(() => Int, { nullable: true })
   accuracyRate?: number;
