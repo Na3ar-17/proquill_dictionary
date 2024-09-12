6e4ca5f24215edd30aecce7b33c287149941b340
Na3ar-17 <gavruk2017@gmail.com>
2024-09-12 11:58:33 +0200

    final commit

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
 
-  // app.setGlobalPrefix('api');
   app.enableCors({
     credentials: true,
     exposedHeaders: 'set-cookie',
