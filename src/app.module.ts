f497dcb45d72fc810096bfbd02c31566ddee61f8
Na3ar-17 <gavruk2017@gmail.com>
2024-09-18 20:35:50 +0200

    implemented .env changing betwen mods

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { AuthModule } from './auth/auth.module';
 import { ContentModule } from './content/content.module';
 import { LearningProgressModule } from './learning-progress/learning-progress.module';
 import { PrismaService } from './prisma/prisma.service';
+import { QuizModule } from './quiz/quiz.module';
 import { ThemeModule } from './theme/theme.module';
 import { UserModule } from './user/user.module';
-import { QuizModule } from './quiz/quiz.module';
+const ENV = process.env.NODE_ENV;
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
@@ -23,7 +24,9 @@ import { QuizModule } from './quiz/quiz.module';
       context: ({ req, res }) => ({ req, res }),
     }),
     UserModule,
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      envFilePath: !ENV ? '.env.development.local' : `.env.${ENV}.local`,
+    }),
     AuthModule,
     ThemeModule,
     ContentModule,
